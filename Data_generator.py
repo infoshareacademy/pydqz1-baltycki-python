f63619729b257ec974621cbad5a47dbc0b5fe4bc
Marcin Babalski <marcin.babalski@gmail.com>
2019-12-08 12:41:04 +0100

    Adding project's template

---
+++ Data_generator.py
@@ -0,0 +1,23 @@
+import random
+import json
+
+user_name = ""
+user_surname = ""
+email = ""
+nick = ""
+address = []
+
+def generate_name():
+    pass
+
+def generate_surname():
+    pass
+
+def generate_email():
+    pass
+
+def generate_nick():
+    pass
+
+def generate_address():
+    pass
