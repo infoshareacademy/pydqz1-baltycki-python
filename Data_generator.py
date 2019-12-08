bc70edc39c6a372d9352d95a915cfab772e4aa24
Marcin Babalski <marcin.babalski@gmail.com>
2019-12-08 13:02:39 +0100

    Adding e-mail generator

--- Data_generator.py
+++ Data_generator.py
@@ -14,10 +14,22 @@ def generate_surname():
     pass
 
 def generate_email():
-    pass
+    email_body = []
+    with open("words.txt", "r") as f:
+        for line in f:
+            line = line.strip('\n')
+            email_body.append(line)
+    domain = [".com", ".org", ".mil", ".net", ".edu", ".pl", ".de", ".fr", ".ie", ".us", ".co.uk"]
+    local_part = email_body[random.randint(0, len(email_body) - 1)]
+    host_name = email_body[random.randint(0, len(email_body) - 1)]
+    mail_domain = (domain[random.randint(0, len(domain) - 1)])
+    full_email = local_part + "@" + host_name + mail_domain
+    return full_email
 
 def generate_nick():
     pass
 
 def generate_address():
     pass
+
+generate_email()
