0c0b25c99f1c27bdcb057e09bc1817d30d1a5ba3
Marcin Babalski <marcin.babalski@gmail.com>
2019-12-08 13:49:00 +0100

    Adding address generation

--- Data_generator.py
+++ Data_generator.py
@@ -26,10 +26,25 @@ def generate_email():
     full_email = local_part + "@" + host_name + mail_domain
     return full_email
 
+email = generate_email()
+
 def generate_nick():
     pass
 
 def generate_address():
-    pass
+    code = []
+    street = []
+    city = []
+    with open("address.txt", "r") as f:
+        for line in f:
+            address_data = line.split(",")
+            code.append(address_data[0])
+            street.append(address_data[1])
+            city.append(address_data[2])
+    city_code = code[random.randint(0, len(code) - 1)]
+    city_name = city[random.randint(0, len(city) - 1)]
+    city_street = street[random.randint(0, len(street) - 1)]
+    full_address = city_code + " " + city_name.strip('\n') + ", " + city_street + " " + str(random.randint(1,100))
+    return full_address
 
-generate_email()
+address = generate_address()
