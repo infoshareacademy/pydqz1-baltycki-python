933f8f296578f34b6d53a9e2118af4664f37c7cd
Mateusz Franckiewicz <mateusz.franckiewicz@outlook.com>
2019-12-13 11:09:15 +0100

    Creat generate_surname.py and added person.txt

---
+++ generate_surname.py
@@ -0,0 +1,10 @@
+from random import choice
+
+
+def surname():
+    surname_list = []
+    with open('person.txt', 'r') as f:
+        for line in f:
+            person = line.split()
+            surname_list.append(person[1])
+    return choice(surname_list)
