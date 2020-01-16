edf2f62aa0df85332e5767615291662824d4ed96
Marcin Babalski <marcin.babalski@gmail.com>
2020-01-16 19:25:51 +0100

    Adding save to json file during generation

--- Data_generator.py
+++ Data_generator.py
@@ -3,6 +3,7 @@ import json
 
 
 def generate_name(ptf=True):
+    name_to_json = {}
     name_list = []
     with open('person.txt', 'r') as f:
         for line in f:
@@ -10,12 +11,14 @@ def generate_name(ptf=True):
             name_list.append(person[0])
     user_name = random.choice(name_list)
     if ptf is True:
-        with open("log.data", "a+") as f:
-            f.write(user_name + "\n")
+        name_to_json["First name"] = user_name
+        with open("data.json", "a+") as f:
+            json.dump(name_to_json, f, indent=2)
     return user_name
 
 
 def generate_surname(ptf=True):
+    surname_to_json = {}
     surname_list = []
     with open('person.txt', 'r') as f:
         for line in f:
@@ -23,12 +26,14 @@ def generate_surname(ptf=True):
             surname_list.append(person[1])
     user_surname = random.choice(surname_list)
     if ptf is True:
-        with open("log.data", "a+") as f:
-            f.write(user_surname + "\n")
+        surname_to_json["Last name"] = user_surname
+        with open("data.json", "a+") as f:
+            json.dump(surname_to_json, f, indent=2)
     return user_surname
 
 
 def generate_email(ptf=True):
+    email_to_json = {}
     email_body = []
     with open("words.txt", "r") as f:
         for line in f:
@@ -40,12 +45,14 @@ def generate_email(ptf=True):
     mail_domain = random.choice(domain)
     e_mail = local_part + "@" + host_name + mail_domain
     if ptf is True:
-        with open("log.data", "a+") as f:
-            f.write(e_mail + "\n")
+        email_to_json["E-mail"] = e_mail
+        with open("data.json", "a+") as f:
+            json.dump(email_to_json, f, indent=2)
     return e_mail
 
 
 def generate_nick(ptf=True):
+    nick_to_json = {}
     nick_list = []
     with open("nick.txt", "r") as f:
         for line in f:
@@ -53,8 +60,9 @@ def generate_nick(ptf=True):
             nick_list.append(line)
     nick = random.choice(nick_list) + str(random.randint(1, 100))
     if ptf is True:
-        with open("log.data", "a+") as f:
-            f.write(nick + "\n")
+        nick_to_json["Nick"] = nick
+        with open("data.json", "a+") as f:
+            json.dump(nick_to_json, f, indent=2)
     return nick
 
 
@@ -76,57 +84,16 @@ def generate_address(ptf=True):
     address["City"] = city_name.strip('\n')
     address["Street"] = city_street + " " + str(random.randint(1, 100))
     if ptf is True:
-        with open("log.data", "a+") as f:
-            save_address = address["Postal code"] + ", " + address["City"] + ", " + address["Street"]
-            f.write(save_address + "\n")
+        with open("data.json", "a+") as f:
+            json.dump(address, f, indent=2)
     return address
 
 
-''' 
-def full_set(user_name, user_surname, nick, e_mail, address):
-    data = {"First name": user_name, "Last name": user_surname,
-            "Nick": nick, "E-mail": e_mail, "Address": address}
-
-    for key, value in data.items():
-        print(key + ": " + value)
+def generate_full_set(ptf=True):
+    data = {"First name": generate_name(ptf=False), "Last name": generate_surname(ptf=True),
+            "Nick": generate_nick(ptf=False), "E-mail": generate_email(ptf=False), "Address": generate_address(ptf=False)}
+    if ptf is True:
+        with open("data.json", "a+") as f:
+            json.dump(data, f, indent=2)
     return data
 
-
-def save_to_json(user_name, user_surname, nick, e_mail, address):
-    import json
-
-    json_data = {"First name": user_name, "Last name": user_surname, "Nick": nick, "E-mail": e_mail,
-                 "Address": address}
-
-    json_string = json.dumps(json_data)
-
-    with open("data.json", "w") as f:
-        f.write(json_string)
-
-
-def save_to_xml(user_name, user_surname, nick, e_mail, address):
-    import xml.etree.ElementTree as xml
-
-    root = xml.Element("Test data")
-    cl = xml.Element("User")
-    root.append(cl)
-
-    name1 = xml.SubElement(cl, "First name")
-    name1.text = user_name
-
-    surname1 = xml.SubElement(cl, "Last name")
-    surname1.text = user_surname
-
-    nick1 = xml.SubElement(cl, "Nick")
-    nick1.text = nick
-
-    email1 = xml.SubElement(cl, "Email")
-    email1.text = e_mail
-
-    address1 = xml.SubElement(cl, "Address")
-    address1.text = address
-
-    tree = xml.ElementTree(root)
-
-    with open("User.xml", "wb") as files:
-        tree.write(files)'''
