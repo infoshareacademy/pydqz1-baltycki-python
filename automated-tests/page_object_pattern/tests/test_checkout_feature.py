6818b7a0d2a418f820d558c59b71dbc401a0843b
Mateusz Franckiewicz <mateusz.franckiewicz@outlook.com>
2020-02-13 12:09:07 +0100

    Added #2 test_registered_user_pay_by_check

--- automated-tests/page_object_pattern/tests/test_checkout_feature.py
+++ automated-tests/page_object_pattern/tests/test_checkout_feature.py
@@ -39,3 +39,29 @@ class TestCheckoutFeature:
         sleep(2)
         assert 'Your order on My Store is complete.' == order_complete, 'No order complete text on page'
         sleep(2)
+
+    def test_registered_user_pay_by_check(self):
+        """Happy path: registered user with one item in cart paid by check"""
+        self.driver.get('http://automationpractice.com/index.php')
+        main_page = MainPage(self.driver)
+        main_page.click_on_product()
+        product_page = ProductPage(self.driver)
+        product_page.add_product_to_cart()
+        cart_summary = CartPageSummary(self.driver)
+        cart_summary.click_checkout_button()
+        cart_sigin = CartPageSignIn(self.driver)
+        cart_sigin.login_as_registered_user()
+        cart_address = CartPageAddresses(self.driver)
+        cart_address.click_checkout_button()
+        cart_shipping = CartPageShipping(self.driver)
+        cart_shipping.enable_checkbox()
+        cart_shipping.click_checkout_button()
+        cart_payment = CartPagePayment(self.driver)
+        cart_payment.click_pay_by_check()
+        cart_order_summary = CartPageOrderSummary(self.driver)
+        cart_order_summary.click_order_confirmation_buton()
+        cart_order_confirmation = CartPageOrderConfirmation(self.driver)
+        order_complete = cart_order_confirmation.get_order_confiramation_text()
+        sleep(2)
+        assert 'Your order on My Store is complete.' == order_complete, 'No order complete text on page'
+        sleep(2)
