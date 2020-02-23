a833d2824e7c87cdd35d2fd4373bf66269435405
Mateusz Franckiewicz <mateusz.franckiewicz@outlook.com>
2020-02-23 08:16:20 +0100

    Changed conftest.py

--- automated-tests/conftest.py
+++ automated-tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-import time
+import allure
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
@@ -9,12 +10,14 @@ from page_object_pattern.pages.shopping_cart import ShoppingCart
 @pytest.fixture()
 def setup(request):
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get('http://automationpractice.com/index.php')
     driver.maximize_window()
     driver.implicitly_wait(10)
+    driver.get('http://automationpractice.com/index.php')
     request.cls.driver = driver
-    request.cls.wait = WebDriverWait(driver, 15)
+    request.cls.wait = WebDriverWait(driver, 10)
     request.cls.shoppingCart = ShoppingCart(driver)
     yield
-    time.sleep(2)
+    before_failed = request.session.testsfailed
+    if request.session.testsfailed != before_failed:
+        allure.attach(driver.get_screenshot_as_png(), name='Test failed', attachment_type=AttachmentType.PNG)
     driver.quit()
