08d40b6cab28f5ee2d2f38c380f3fef234d71544
Mateusz Franckiewicz <mateusz.franckiewicz@outlook.com>
2020-02-25 20:55:39 +0100

    Added search tests

--- automated-tests/page_object_pattern/pages/searching_products.py
+++ automated-tests/page_object_pattern/pages/searching_products.py
@@ -6,10 +6,23 @@ class SearchingProducts:
     def __init__(self, driver):
         self.driver = driver
 
-    def search_box_is_empty(self):
-        search_bar = self.driver.find_element(*Locators.SEARCH_BAR).text
-        return search_bar
+    def search_bar_is_displayed(self):
+        search_bar = self.driver.find_element(*Locators.SEARCH_BAR)
+        return search_bar.is_displayed()
 
-    def enter_query_to_search_input(self, query):
+    def enter_query_to_search_bar(self, query):
         self.driver.find_element(*Locators.SEARCH_BAR).send_keys(query)
         self.driver.find_element(*Locators.SEARCH_GO_BUTTON).click()
+        search_counter = self.driver.find_element(*Locators.SEARCH_COUNTER)
+        return search_counter.text
+
+    def get_search_counter(self):
+        return self.driver.find_element(*Locators.SEARCH_COUNTER).text
+
+    def search_alert_is_displayed(self):
+        search_alert = self.driver.find_element(*Locators.SEARCH_ALERT)
+        return search_alert.is_displayed()
+
+    def list_of_products(self):
+        products_list = self.driver.find_elements_by_css_selector('div.product-container')
+        return products_list
--- automated-tests/page_object_pattern/tests/test_searching_products.py
+++ automated-tests/page_object_pattern/tests/test_searching_products.py
@@ -1,8 +1,26 @@
 import pytest
+import allure
 
 
+@allure.description('Testing search feature')
 @pytest.mark.usefixtures('setup')
 class TestSearchingProducts:
 
-    def test_search_empty_query(self, setup):
-        assert '' == self.searching_products.search_box_is_empty()
+    def test_search_bar_is_displayed(self, setup):
+        assert self.searching_produtcs.search_bar_is_displayed() is True
+
+    def test_empty_search_query(self, setup):
+        assert self.searching_produtcs.search_bar_is_displayed() is True
+        assert '0 results' in self.searching_produtcs.enter_query_to_search_bar('')
+        assert self.searching_produtcs.search_alert_is_displayed() is True
+
+    def test_search_tshirt_query(self, setup):
+        assert '1 result' in self.searching_produtcs.enter_query_to_search_bar('t-shirt')
+        assert len(self.searching_products.list_of_products()) > 0
+
+    # def test_search_dress_query(self, setup):
+    #     assert self.searching_produtcs.search_bar_is_displayed() is True
+    #     product_counter = str(len(self.searching_produtcs.list_of_products()))
+    #     assert '7 result' in self.searching_produtcs.enter_query_to_search_bar('dress')
+    #     assert 'Search - My Store' == self.driver.title
+    #     assert '7' == product_counter
