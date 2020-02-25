7c1e47dab675d8a279ec002bc59a4a6c689f8653
Mateusz Franckiewicz <mateusz.franckiewicz@outlook.com>
2020-02-25 21:47:16 +0100

    Modified conftest.py & requirements.txt

--- automated-tests/conftest.py
+++ automated-tests/conftest.py
@@ -1,20 +1,24 @@
 import pytest
+import allure
+from allure_commons.types import AttachmentType
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from page_object_pattern.pages.shopping_cart import ShoppingCart
+from page_object_pattern.pages.searching_products import SearchingProducts
 
 
 @pytest.fixture()
 def setup(request):
-# te akcje zostsaną wykonane przed testem:
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.implicitly_wait(10)
     driver.maximize_window()
+    driver.implicitly_wait(10)
+    driver.get('http://automationpractice.com/index.php')
     request.cls.driver = driver
-    """It's easy - request.cls is the test class using the fixture, so request.cls.driver = ...
-    is essentially the same as MyTestClass.driver = ... if MyTestClass uses the fixture.
-    https://pytest.readthedocs.io/en/2.8.7/builtin.html"""
-
-# te akcje zostaną po wykonaniu akcji z plików z testami:
+    request.cls.shoppingCart = ShoppingCart(driver)
+    request.cls.searching_produtcs = SearchingProducts(driver)
+    before_failed = request.session.testsfailed
     yield
+    if request.session.testsfailed != before_failed:
+        allure.attach(driver.get_screenshot_as_png(), name='Test failed', attachment_type=AttachmentType.PNG)
     driver.quit()
-
