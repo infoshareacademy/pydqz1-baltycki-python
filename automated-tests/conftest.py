93d9763ba20b701f2c098413419cc4f7a739fd2f
Mateusz Franckiewicz <mateusz.franckiewicz@outlook.com>
2020-04-02 02:02:03 +0200

    Updated conftest

--- automated-tests/conftest.py
+++ automated-tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 import allure
 from allure_commons.types import AttachmentType
-import time
+from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from page_object_pattern.pages.shopping_cart import ShoppingCart
+from page_object_pattern.pages.checkout_feature import CheckoutFeature
 from page_object_pattern.pages.searching_products import SearchingProducts
+from page_object_pattern.pages.shopping_cart_page import ShoppingCart
 
 
 @pytest.fixture()
@@ -15,10 +16,12 @@ def setup(request):
     driver.implicitly_wait(10)
     driver.get('http://automationpractice.com/index.php')
     request.cls.driver = driver
+    request.cls.checkout_feature = CheckoutFeature(driver)
+    request.cls.searching_products = SearchingProducts(driver)
     request.cls.shoppingCart = ShoppingCart(driver)
-    request.cls.searching_produtcs = SearchingProducts(driver)
     before_failed = request.session.testsfailed
     yield
     if request.session.testsfailed != before_failed:
         allure.attach(driver.get_screenshot_as_png(), name='Test failed', attachment_type=AttachmentType.PNG)
+    sleep(2)
     driver.quit()
